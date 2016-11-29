b4e2e603b6f20548be085977b92d6068b7b26e22
samzhu <spike19820318@gmail.com>
2016-11-29 15:56:31 +0800

    Update YysportsItemServiceApplicationTests.java

--- src/test/java/com/yysports/YysportsItemServiceApplicationTests.java
+++ src/test/java/com/yysports/YysportsItemServiceApplicationTests.java
@@ -16,9 +16,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles(profiles = "dev")
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@ActiveProfiles(profiles = "dev")
 public class YysportsItemServiceApplicationTests {
 
     @Autowired
